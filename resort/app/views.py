af29b4c714acab052f0fa807b5414f1e84a077ea
BiTech5 <bikikc7@gmail.com>
2024-06-29 11:16:19 +0545

    form to model data

--- resort/app/views.py
+++ resort/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 # Create your views here.
@@ -18,6 +18,19 @@ def add_data(request):
         package=request.POST.get('package')
         tariff=request.POST.get('tariff')
         advance=request.POST.get('advance')
-        remark=request.POST.get('remark')
-        print(name)
+        remark=request.POST.get('remarks')
+        #instance for model data
+        customer=CustomerModel(name=name,
+                               contect=contact,
+                               arrivals=arrival,
+                               departure=departure,
+                               pax=pax,
+                               stay_type=stay,
+                               package=package,
+                               tariff=tariff,
+                               advance=advance,
+                               remarks=remark)
+        customer.save()
+        return redirect('home')
+        
     return render(request,'form.html')
