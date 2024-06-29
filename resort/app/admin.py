bb486faa4a20cbd1b376d79e80333912a2bb8c06
BiTech5 <bikikc7@gmail.com>
2024-06-29 12:18:34 +0545

    making the import option in model

--- resort/app/admin.py
+++ resort/app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from import_export.admin import ImportExportActionModelAdmin
 from .models import *
 # Register your models here.
-admin.site.register(CustomerModel)
+class CustomerAdmin(ImportExportActionModelAdmin):
+    list_display=['name']
+admin.site.register(CustomerModel,CustomerAdmin)
